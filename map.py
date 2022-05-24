f55f59dee31dc164951c993862c0d6bf8b7381d7
Piotr Kitłowski <piotr.kitlowski@gmail.com>
2022-05-24 19:23:54 +0200

    Added map generation, not ideal though

--- map.py
+++ map.py
@@ -3,21 +3,23 @@
 import pygame
 import os
 import multiprocessing as mp
+
 #import networkx as nx
 
 FPS = 60
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1920, 1080
 # Nodes radius
 RADIUS = 35
 # Distance between intersections
-DISTANCE = 50
 WHITE = (255, 255, 255)
 DEEPBLUE = (0, 191, 255)
 GREEN = (0, 250, 80)
 DARK = (0, 0, 0)
 DIMGRAY = (105, 105, 105)
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Graph")
+FLAGS = pygame.FULLSCREEN | pygame.SCALED
+#FLAGS,
+WIN = pygame.display.set_mode((WIDTH, HEIGHT), vsync=1)
+pygame.display.set_caption("Simulation")
 
 pygame.init()
 FONT = pygame.font.SysFont('arial', 40)
@@ -44,49 +46,103 @@ class intersection(pygame.sprite.Sprite):
       if(self.outgoing[i][1] == dest):
         self.outgoingLock.release()
         return i
-    self.outgoingLock.release()  
-    return NULL
+    self.outgoingLock.release()
+    return None
   def getFirstOnRoadTo(self, dest):
     self.outgoingLock.acquire()
     for i in range(len(self.outgoing)):
       if(self.outgoing[i][1] == dest):
         self.outgoingLock.release()
         return i
-    self.outgoingLock.release()  
-    return NULL
-
+    self.outgoingLock.release()
+    return None
   def __init__(self, name, position, neighbors, weights):
-    
+
     self.name = name
     self.position = position
     self.neighbors = neighbors
     self.weights = weights
     #self.font = FONT
+
     self.outgoingManager = mp.Manager()
     self.outgoingLock = self.outgoingManager.Lock()
     self.outgoing = self.outgoingManager.list() #car, going to
+
   def __getitem__ (self, key):
     return getattr(self, key)
   #def add_text_to_map(self):
 
 
+def map_helper(__map, __name, __position, __neighbors, __weights, __node_name):
+  __map.append(intersection(__name, __position, __neighbors, __weights))
+  __node_name = __node_name + 1
+  return __map, __node_name
+
 # distance_between_intersections -> dbi
-def generate_map(num_of_intersections, dbi, is_fully_connected):
-  _map = []
+# num_of_intersections -> noi
+def generate_map(noi, dbi, offset = 0, is_fully_connected = True):
+  __map = []
+  __node_name = 0
+  __current_pos = 0
+  for idx in range(noi):
+    for idy in range(noi):
+      __current_pos = idx*noi + idy
+      __name = f"v{__node_name}"
+      __position = (dbi*idx + offset, dbi*idy + offset)
 
-  for idx in range(num_of_intersections):
-    _name = f"v{idx}"
-    # generate positions
-    _position = (0, 0)
-    # generate neigbors
-    _neighbors = []
-    # generate weights
-    _weights = []
-    _map.append(intersection(_name, _position, _neighbors, _weights))
+      #is_border_intersection = False
+      __neighbors = []
+      __weights = []
 
-  return _map
+      # Corners
+      """
+      if(idx == 0 and idy == 0):
+        print(f"X {idx}, Y {idy}")
+        __neighbors.extend([1, noi])
+        __map, __node_name = map_helper(__map, __name, __position, __neighbors, __weights, __node_name)
+
+      elif(idx == 0 and idy == noi-1):
+        print(f"X {idx}, Y {idy}")
+        __neighbors.extend([noi-2, 2*noi-1])
+        __map, __node_name = map_helper(__map, __name, __position, __neighbors, __weights, __node_name)
+
+      elif(idx == noi-1 and idy == 0):
+        print(f"X {idx}, Y {idy}")
+        __neighbors.extend([noi*(noi-2), noi*(noi-1)+1])
+        __map, __node_name = map_helper(__map, __name, __position, __neighbors, __weights, __node_name)
+
+      elif(idx == noi-1 and idy == noi-1):
+        print(f"X {idx}, Y {idy}")
+        __neighbors.extend([noi*(noi-1)-1, noi**2-2])
+        __map, __node_name = map_helper(__map, __name, __position, __neighbors, __weights, __node_name)
+      """
+      # Other borders
+      #elif(idx == 0 and 1 <= idy <= noi-2):
+      #  __neighbors.extend([idy-1, idy+1, idy+noi])
+      #  __map, __node_name = map_helper(__map, __name, __position, __neighbors, __weights, __node_name)
+
+      #elif(1 <= idx <= noi-2 and idy == 0):
+      #  print(f"X {idx}, Y {idy}")
+        #__neighbors.extend([])
+        #__map, __node_name = map_helper(__map, __name, __position, __neighbors, __weights, __node_name)
+      #elif(1 <= idx <= noi-2 and idy == noi-2):
+      #elif(idx == noi-2 and 1 <= idy <= noi-2):
+
+      #else:
+      #if(idx*noi):
+      #  print(f"current position {}")
+
+      __neighbors.extend([__current_pos-noi, __current_pos-1, __current_pos+1, __current_pos+noi])
+      __map, __node_name = map_helper(__map, __name, __position, __neighbors, __weights, __node_name)
 
 
+
+
+  return __map
+# x->
+#y# "v00", "v10",
+#|# "v01", "v11"
+#v
 map = [
   #            name   position    neighbors    weights
   intersection("v0",  (50, 50),   [1, 4],      [2, 3]),
@@ -108,6 +164,45 @@ map = [
   intersection("v15",  (350, 250), [14,  12],  [1, 1]),
 ]
 
+def draw_map(map_):
+  WIN.fill(GREEN)
+  pygame.display.flip()
+  __visited = []
+  for i in range(len(map_)):
+    #font = pygame.font.sysFont(map["name"], 30)
+    # Get node position and draw
+    x, y = map_[i]["position"]
+    WIN.blit(FONT.render(map_[i].name, True, (WHITE)), (x+10, y+10))
+    # Draw Nodes
+    # pygame.draw.circle(WIN, DEEPBLUE,
+    #                   (x, y), RADIUS)
+
+
+    for neighbor in map_[i]["neighbors"]:
+      __edge = []
+      __edge.append(i)
+      __edge.append(neighbor)
+      __edge = sorted(__edge)
+      __visited.append(__edge)
+
+      if(__visited.count(__edge) > 1):
+        pygame.draw.line(WIN, DIMGRAY, map_[i]["position"],
+                                    map_[neighbor]["position"],
+                                    30 )
+        pygame.draw.line(WIN, WHITE, map_[i]["position"],
+                                    map_[neighbor]["position"],
+                                    2 )
+        #print(f"[{i}] and [{neighbor}] DOUBLE EDGE")
+      """
+      else:
+        pygame.draw.line(WIN, DIMGRAY, map_[i]["position"],
+                                    map_[neighbor]["position"],
+                                    30 )
+      """
+        #print(f"[{i}] and [{neighbor}] SINGLE EDGE")
+
+    # Update screen
+    pygame.display.update()
 
 
 def draw_window():
@@ -160,7 +255,12 @@ def main():
   clock = pygame.time.Clock()
   run = True
 
-  draw_window()
+  new_map = []
+
+  new_map = generate_map(5, 200, 50)
+  draw_map(new_map)
+
+  #draw_window()
 
   while run:
     clock.tick(FPS)
